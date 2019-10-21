ab72940a1adc53575da5f8e37468033701a16317
Yuta Fujii <yutayutakid21@gmail.com>
2019-10-21 13:45:58 +0900

    print(”あいうえお”)、かきくけこを追加しました！

--- yutayutaProject1/ViewController.swift
+++ yutayutaProject1/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("あいうえお！！")
+        print("かきくえこ！")
+        
         // Do any additional setup after loading the view.
     }
 
