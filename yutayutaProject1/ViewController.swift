4060742721c93679043ff4be9589925160151b25
Yuta Fujii <yutayutakid21@gmail.com>
2019-10-21 13:44:36 +0900

    Initial Commit

---
+++ yutayutaProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yutayutaProject1
+//
+//  Created by Fujii Yuta on 2019/10/21.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
